a970668b1a880c66733bc63d1c089bf298f3dcdf
Furqan Mehmood <furqanizgenius@gmail.com>
2025-08-16 23:31:07 +0500

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+This is my First Repository.
+<br/>
+Author - Furqan Mehmood.
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>This is a Demo Repository</h1>
+      <h2>Learning GIT and GITHub using Terminal and GIT Bash</h2>
+      <h3>Furqan Mehmood</h3>
+    </>
+  )
+}
+
+export default App
